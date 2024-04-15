5e9257ff653d8ae233d0d2f6ecd71f827235f9a0
Woze Parrot <wozeparrot@gmail.com>
2024-04-15 15:26:21 -0400

    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "created-gravy: A vanilla-esque modpack centered around Create.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            packwiz
+          ];
+        };
+      }
+    );
+}
